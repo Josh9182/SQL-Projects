7d14e54e1e09903772a706117b0b4f47232eb32e
Josh Lewis <jlewis200413@gmail.com>
2024-02-14 13:56:27 -0700

    Update customer_database.sql

--- SQL-Portfolio/customer_database.sql
+++ SQL-Portfolio/customer_database.sql
@@ -1,1 +1,22 @@
+﻿-- Table: customer_database
 
+CREATE TABLE customer_database
+(
+  Age numeric,
+  Gender character varying(30),
+  Time_Spent_Hrs numeric,
+  Dominant_Platform text,
+  Interests text,
+  Location text,
+  Demographics character varying(20),
+  Profession text,
+  Annual_Income numeric,
+  In_Debt character varying(5),
+  Debt_Amount numeric,
+  Is_Homeowner character varying(5),
+  Owns_Car character varying(5)  
+);
+
+
+-- Copy over data from CSV
+\copy customer_database FROM 'customer_database.csv' DELIMITER ',' CSV HEADER;
