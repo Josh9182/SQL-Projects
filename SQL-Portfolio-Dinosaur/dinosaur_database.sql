d0f8e87d17197d8012187107a5d8cd20cd18b38c
Josh Lewis <jlewis200413@gmail.com>
2024-03-01 09:56:05 -0700

    Update dinosaur_database.sql

--- SQL-Portfolio-Dinosaur/dinosaur_database.sql
+++ SQL-Portfolio-Dinosaur/dinosaur_database.sql
@@ -1,4 +1,4 @@
-﻿-- Table: dino_data
+﻿-- Table: dino_database
 
 CREATE TABLE dino_database AS
 SELECT 
@@ -16,3 +16,6 @@ FROM
     dino_biological_info bio
 JOIN 
     dino_archaeological_info arch ON bio.Name = arch.Name;
+
+-- Copy over data from CSV
+\copy dino_database FROM 'customer_database.csv' DELIMITER ',' CSV HEADER;
