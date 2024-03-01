cad5659b3a844c1579e67e7d05a47e99655ee43d
Josh Lewis <jlewis200413@gmail.com>
2024-03-01 09:52:12 -0700

    Update dinosaur_database.sql

--- SQL-Portfolio-Dinosaur/dinosaur_database.sql
+++ SQL-Portfolio-Dinosaur/dinosaur_database.sql
@@ -1,18 +1,18 @@
 ﻿-- Table: dino_data
 
-CREATE TABLE dino_data
-(
-  Name character varying(50),
-  Diet character varying(15),
-  Period character varying(200),
-  Lived_in character varying(30),
-  Type character varying(30),
-  Length character varying(10),
-  Taxonomy character varying(200),
-  Named_By character varying(200),
-  Species character varying(30)
-);
-
-
--- Copy over data from CSV
-\copy dino_data FROM 'dino_data.csv' DELIMITER ',' CSV HEADER;
+CREATE TABLE dino_database AS
+SELECT 
+    bio.Name,
+    bio.Diet,
+    bio.Length,
+    bio.Taxonomy,
+    bio.Species,
+    arch.Name,
+    arch.Type,
+    arch.Period,
+    arch.Lived_in,
+    arch.Named_By
+FROM 
+    dino_biological_info bio
+JOIN 
+    dino_archaeological_info arch ON bio.Name = arch.Name;
